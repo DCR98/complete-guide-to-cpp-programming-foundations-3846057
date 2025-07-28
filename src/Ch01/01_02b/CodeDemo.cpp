d2092763ae445ad626b436b9f66770e476ddad30
DCR98 <rocodcm@gmail.com>
2025-07-28 03:05:47 +0000

    no space

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -7,9 +7,8 @@
 int add(int a, int b){
     return a + b;
 }
-
-int main(){ //cant understand the functions lol
+int main(){
     int result = add(5,3);
-    std::cout<< "The sum is =" << result << std::endl;
+    std::cout<< "The sum is = " << result << std::endl;
     return 0;
 }
