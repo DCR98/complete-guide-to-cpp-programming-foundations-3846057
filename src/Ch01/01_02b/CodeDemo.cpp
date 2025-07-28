879f64619e0e13fc0f6aef3760c7ab0e3e71afa8
DCR98 <rocodcm@gmail.com>
2025-07-28 02:49:19 +0000

    tried multiple examples

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,15 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 01_02
+// Exercise 01_02 with changes
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int add(int a, int b){
+    return a + b;
+}
+
+int main(){ //cant understand the functions lol
+    int result = add(5,3);
+    std::cout<< "The sum is =" << result << std::endl;
+    return 0;
+}
